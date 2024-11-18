6dd77d9d65df0ae22b97e06ad3b2e324bcd13a03
sist102 <sist102@DESKTOP-06VP6JK>
2024-11-18 14:19:58 +0900

    0000

---
+++ firstproject/src/firstproject/자바01.java
@@ -0,0 +1,61 @@
+package firstproject;
+
+public class 자바01 {
+/*
+	1.대문자 구분
+	2.들여쓰기  => 가독성
+	3.명령문이 종료 반드시 ;
+	 ;사용 안함 => 파이썬/kotlin/javasscript
+	4.자바는 {괄호가 몊에 붙는다}
+	5.기본 문법
+	  데이터 저장 => 데이터 처리(요청) => 회면에 출력
+	     ㅣ				ㅣ				ㅣ
+	     		   연산자 / 제어문		명령프롬프트(문법 배움) : Application
+	     		   ===========		브라우저 출력(주력으로 배움) : Web Application
+	     		    	ㅣ			모바일 출력 : Mobile Application
+	     		       메소드			 Spring : Java / Kotlin
+	     한개 저장 (메모리):변수.상수
+	     여러개 저장 : 배열
+	     			클래스
+	   ------------------------
+	   프로그램 종료시에 사라진다
+	   파일. 오라클 =>영구적 저장
+	 5-2기능을 모아서 관리
+	 =>단락 : 메소드
+	 =>클래스 / 객체 => 객체지향 프로그램
+	 =>프로그램 유지 (예외처리) : 8장
+	 =>라이브러리 : 자바에 지원
+	 =>1차 응용프로그램 (총 4개정도 만들어봄)
+	 => Web
+	 1.HTML : 브라우저에서 실행
+	 		  자바는 부라우저에서는 일반 텍스트
+	 		=> css / 자바스크립트
+	 2. JSP (요청 HTMl JAVA
+	 		응답 JAVA HTML)
+	 	Front Back = fullstack	
+	 3. 통합 : Framework : Spring
+	 		  기본 틀 => 메인보드
+	 4.데이터 분석 :  Python / Numpy / Pandas / MatlpotLib (여기까지 ai 전처리과정)
+	 5.NoSql => elasticsearch (오라클 역할) [구글이 이것으로 만듬 속도가 빨라서]
+	 6.배포 : AWS
+	 	5.5개월 걸릴것으로 예상
+	 7.그외 Spring boot / JPA / MySQL / React - Query
+	 Redux / Vue3 / NextJS / TypeScript / NodeJS
+	 
+ 		자바 / 오라클 / 알고리즘 총 3권의 책
+ 		HTML CSS J / JSP
+ 		Spring-boot / Python / 데이터 분석
+ 		elacticsearch
+ 	실무는 Spring + MyBatis + JSP + JavaScript
+ 	
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java");
+		System.out.println("김요한");
+		{
+			
+		}
+	}
+
+}
