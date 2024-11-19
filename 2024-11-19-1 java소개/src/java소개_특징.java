e6700f66484c0882b7c251841ab3358f3afd1db7
sist102 <sist102@DESKTOP-06VP6JK>
2024-11-19 13:48:32 +0900

    2024-11-19-자바소개 및 변수1

---
+++ 2024-11-19-1 java소개/src/java소개_특징.java
@@ -0,0 +1,59 @@
+
+public class java소개_특징 {
+/*자바 배우는 과정 문법 1~6
+1.변수 : 데이터형 (메모리에 저장하는 크기)
+데이터 저장 : 메모리에 저장 > ram (프로그램 종료시 삭제)\=>파일 / rdbms(오라클)
+2.사용자 요청에 따라 처리 (데이터 처리)
+=>연산자 / 제어문
+3.여러개를 동시에 저장
+ =>같은 데이터만 저장 (정수끼리,실수끼리) 배열이라 부름
+ =>다른 데이터 저장 : 클래스(*******)
+                                  ㅣ재사용 (수정/추가)
+(바는 재사용,관리가 편안하다. 그래서 사용한다.)
+
+4.데이터 모아서 관리 = 배열 , 클래스
+명령문을 모아서 관리 = 메소드
+5.객체 지향 프로그램
+프로그램 원칙/목적
+1.가독성
+2.재사용성
+3.최적화 = 속도를 올리기 위함(리엑트,뷰js사용이유),사용성 ex)네이버,카카오로 회원가입,로그인
+6.예외처리 = 종료하지 않는 프로그램 (계산기 0으로 나누기)
+7.라이브러리 = 자바에서 지원하는 기능 (클래스/메소드)
+io/네트워크
+web 프로그램 기반
+8.미니프로젝트를 만듬 - 데이터는 파일기반으로 만듬
+ 2page
+자바 실행 과정
+프로그램 언어 : c/c++,java,c#,python..등
+자바언어는 프로그래머만 인식하는 언어이다
+컴퓨터가 인식할 수 있게 변경하는 과정=컴파일
+모든 문자마다 번호를 가지고 있다. (unicode ex 'a' - 65 - 100001
+0~65535개를 가지고 있다
+c/c++ - a.c - a.obj - a.exe
+                     l 컴파일 된 파일
+                    ㅣ 바이트 코드 = 모든 운영체제에서 동일하게 사용
+                    ㅣ 배포
+java -a.java -- a.class-----------화면출력
+                ㅣjavac        ljava 한줄씩 읽어서 출력=인터프리터 라고 부름
+소스 검증(2번 컴파일 / 실행시 ) 보안이 뛰어나다고 하는 이유
+제임스 고슬링 : 자바는 절대 해킹이 어렵다
+
+5page 이미지 참조
+**이클립스는 코딩시마다 자동 컴파일이 된다.
+ * 
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello"); 
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+	}
+
+}
