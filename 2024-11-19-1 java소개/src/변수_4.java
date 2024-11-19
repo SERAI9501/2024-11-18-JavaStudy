b3e22fa338fb6ebcbbd45fefeb38bac629bfcb58
sist102 <sist102@DESKTOP-06VP6JK>
2024-11-19 17:52:10 +0900

    변수4~5 정리

---
+++ 2024-11-19-1 java소개/src/변수_4.java
@@ -0,0 +1,23 @@
+/*
+ * int a=10;
+ * int b=20;
+ * 
+ * a=b;
+ * b=a;
+ * 
+ * a=?
+ * b=?
+ * 
+ * int temp=a;
+ * a=b
+ * b=temp
+ * 교환할때는 임시 변수를 넣어 줘야 한다.
+ */
+public class 변수_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
