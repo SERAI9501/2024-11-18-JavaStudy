30139d974e2d1a1343ade31cc69357573252adc4
sist102 <sist102@DESKTOP-06VP6JK>
2024-11-21 16:48:18 +0900

    1

--- 2024-11-21/src/자바제어문_단일조건문6.java
+++ 2024-11-21/src/자바제어문_단일조건문6.java
@@ -10,7 +10,7 @@ public class 자바제어문_단일조건문6 {
 		if(score<60)
 			System.out.println("불합격");
 		
-		System.out.println(score>=60?"합격":"불합격");
+		System.out.println(score>=60?"합격":"불합격");//if 문 2개는 삼항연산자를 사용 할 수 있다.
 	}
 
 }
